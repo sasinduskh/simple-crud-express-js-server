cb2f505555189268bfcae8eda2c108a7358228ec
Sasindu K <110117611+sasinduskh@users.noreply.github.com>
2022-10-11 15:12:03 +0530

    Remove my mongodb URL

--- Config.js
+++ Config.js
@@ -1,4 +1,4 @@
-const MONGO_DB_URL = 'mongodb+srv://root:oa2jHg8vIFyDeFJ8@cluster0.nx61o5q.mongodb.net/mern-crud-db?retryWrites=true&w=majority'
+const MONGO_DB_URL = ENETER_YOUR_MONGO_URL
 
 
 module.exports = {MONGO_DB_URL}
